752be6597a16149ad71b52f28b1fedcc5a11b837
afrin823 <afrin12490@gmail.com>
2024-04-24 22:52:42 +0600

    header done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,36 +10,28 @@ const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <li className="text-2xl"><a>Home</a></li>
+        <li className="text-2xl"><a>Services</a></li>
+        <li className="text-2xl"><a>Resume</a></li>
+        <li className="text-2xl"><a>Project</a></li>
+        <li className="text-2xl"><a>Blog</a></li>       
+        <li className="text-2xl"><a>Contact</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-4xl font-bold">Afrin</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <li className="text-2xl"><a>Home</a></li>
+        <li className="text-2xl"><a>Services</a></li>
+        <li className="text-2xl"><a>Resume</a></li>
+        <li className="text-2xl"><a>Project</a></li>
+        <li className="text-2xl"><a>Blog</a></li>       
+        <li className="text-2xl"><a>Contact</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn btn-primary text-white text-xl px-4">Download Resume</a>
   </div>
 </div>
         </div>
